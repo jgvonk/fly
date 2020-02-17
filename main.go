7db59272235a3e1bb0843b047f90c7d6dfa6af21
Jaap Vonk <jaap@vonkjes.com>
2020-02-17 20:55:09 +0100

    initial fly module

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi from mod")
+	// drone := tello.NewDriver("8888")
+
+	// work := func() {
+	//         drone.TakeOff()
+
+	//         gobot.After(5*time.Second, func() {
+	//                 drone.Land()
+	//         })
+	// }
+
+	// robot := gobot.NewRobot("tello",
+	//         []gobot.Connection{},
+	//         []gobot.Device{drone},
+	//         work,
+	// )
+
+	// robot.Start()
+}
