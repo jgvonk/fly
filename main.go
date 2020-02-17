bfbe6c51121c9767d24a5b500a43d43206a48b6e
Jaap Vonk <jaap@vonkjes.com>
2020-02-17 23:06:26 +0100

    add webserver

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Unhandled route: \"%s\"", r.URL.Path[1:])
+	fmt.Printf("Unhandled route: \"%s\"\n", r.URL.Path[1:])
+}
+
+func takeoff(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "takeoff")
+	fmt.Println("takeoff")
+}
+
+func topos(w http.ResponseWriter, r *http.Request) {
+	position := r.URL.Path[7:]
+	fmt.Fprintf(w, "to position %s", position)
+	fmt.Printf("to position %s\n", position)
+}
+
+func land(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "land")
+	fmt.Println("land")
+}
 
 func main() {
-	fmt.Println("Hi from mod")
-	// drone := tello.NewDriver("8888")
-
-	// work := func() {
-	//         drone.TakeOff()
-
-	//         gobot.After(5*time.Second, func() {
-	//                 drone.Land()
-	//         })
-	// }
-
-	// robot := gobot.NewRobot("tello",
-	//         []gobot.Connection{},
-	//         []gobot.Device{drone},
-	//         work,
-	// )
-
-	// robot.Start()
+	http.HandleFunc("/takeoff/", takeoff)
+	http.HandleFunc("/topos/", topos)
+	http.HandleFunc("/land/", land)
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
